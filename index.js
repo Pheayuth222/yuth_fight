f12966a61de3bf5c18bc33699c71ddb4ffee7012
yenpheayuth2222 <yenpheayuth2222@gmail.com>
2023-08-08 13:47:59 +0700

    Added new Design

--- index.js
+++ index.js
@@ -1,65 +1,15 @@
-const dropdownBtn = document.querySelectorAll(".dropdown-btn");
-const dropdown = document.querySelectorAll(".dropdown");
-const hamburgerBtn = document.getElementById("hamburger");
-const navMenu = document.querySelector(".menu");
-const links = document.querySelectorAll(".dropdown a");
-
-function setAriaExpandedFalse() {
-  dropdownBtn.forEach((btn) => btn.setAttribute("aria-expanded", "false"));
-}
-
-function closeDropdownMenu() {
-  dropdown.forEach((drop) => {
-    drop.classList.remove("active");
-    drop.addEventListener("click", (e) => e.stopPropagation());
+const button = document.querySelector(".main-circle");
+const circles = document.querySelectorAll(".circles");
+const cross = document.querySelector(".cross");
+const crossbg = document.querySelector(".bg");
+const boxicons = document.querySelectorAll(".icons svg");
+button.addEventListener("click", function() {
+  cross.classList.toggle("show");
+  crossbg.classList.toggle("show");
+  circles.forEach(element => {
+    element.classList.toggle("show");
   });
-}
-
-function toggleHamburger() {
-  navMenu.classList.toggle("show");
-}
-
-dropdownBtn.forEach((btn) => {
-  btn.addEventListener("click", function (e) {
-    const dropdownIndex = e.currentTarget.dataset.dropdown;
-    const dropdownElement = document.getElementById(dropdownIndex);
-
-    dropdownElement.classList.toggle("active");
-    dropdown.forEach((drop) => {
-      if (drop.id !== btn.dataset["dropdown"]) {
-        drop.classList.remove("active");
-      }
-    });
-    e.stopPropagation();
-    btn.setAttribute(
-      "aria-expanded",
-      btn.getAttribute("aria-expanded") === "false" ? "true" : "false"
-    );
+  boxicons.forEach(element => {
+    element.classList.toggle("colorchange");
   });
 });
-
-// close dropdown menu when the dropdown links are clicked
-links.forEach((link) =>
-  link.addEventListener("click", () => {
-    closeDropdownMenu();
-    setAriaExpandedFalse();
-    toggleHamburger();
-  })
-);
-
-// close dropdown menu when you click on the document body
-document.documentElement.addEventListener("click", () => {
-  closeDropdownMenu();
-  setAriaExpandedFalse();
-});
-
-// close dropdown when the escape key is pressed
-document.addEventListener("keydown", (e) => {
-  if (e.key === "Escape") {
-    closeDropdownMenu();
-    setAriaExpandedFalse();
-  }
-});
-
-// toggle hamburger menu
-hamburgerBtn.addEventListener("click", toggleHamburger);
